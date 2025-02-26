6c767272a0cc52471fc35449eeb64b918846e1f0
Miceal23 <McCullough-M26@ulster.ac.uk>
2025-02-26 15:12:46 +0000

    .
    .

--- Assets/Scripts/GemSpawner.cs
+++ Assets/Scripts/GemSpawner.cs
@@ -5,7 +5,7 @@ public class GemSpawner : MonoBehaviour
 
     public GameObject Gem;
 
-    public void SpawnGems()
+    public void Start()
     {
         int spawnPointX = Random.Range(-11, 11);
         int spawnPointY = Random.Range(1, 1);
@@ -14,7 +14,17 @@ public class GemSpawner : MonoBehaviour
         Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
 
         Instantiate(Gem, spawnPosition, Quaternion.identity);
-
     }
+    //public void SpawnGems()
+    //{
+    //    int spawnPointX = Random.Range(-11, 11);
+    //    int spawnPointY = Random.Range(1, 1);
+    //    int spawnPointZ = Random.Range(-11, 11);
+
+    //    Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
+
+    //    Instantiate(Gem, spawnPosition, Quaternion.identity);
+
+    //}
 
 }
