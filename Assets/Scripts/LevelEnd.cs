b124257f7e227b6837aabb432e00e1b304a99d65
Micheal McCullough <McCullough-M26@ulster.ac.uk>
2025-03-04 15:12:09 +0000

    .

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -5,13 +5,20 @@ public class LevelEnd : MonoBehaviour
 {
     [SerializeField] Timer timer;
     [SerializeField] Gems gems;
+    public Player player;
 
+    private void Start()
+    {
+        player = GetComponent<Player>();
+    }
     void Update()
     {
        if(timer.remainingTime == 0)
-       {    
-
+       {
+            //Disable character controller
             // in here will be something comparing worm and player gems. player gems => worm gems. player wins. else = player lose
-       }
+
+            //https://www.youtube.com/watch?v=TVSLCZWYL_E
+        }
     }
 }
